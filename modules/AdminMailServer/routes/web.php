8edaf0e63d50889e7b02109de03f5407a4bb5e4c
mohamed elshamy <64151860+mohamedElshamy00000@users.noreply.github.com>
2025-10-11 22:12:09 +0300

    Fix: Resolve final duplicate route name for settings

--- modules/AdminMailServer/routes/web.php
+++ modules/AdminMailServer/routes/web.php
@@ -7,16 +7,14 @@ use Modules\AdminMailServer\Http\Controllers\AdminMailServerController;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::middleware(['web', 'auth'])->group(function () {
     Route::group(["prefix" => "admin/settings"], function () {
-        Route::resource('mail-server', AdminMailServerController::class)->only(['index'])->names('admin.settings');
+        
+        Route::resource('mail-server', AdminMailServerController::class)
+            ->only(['index'])
+            ->names('admin.settings.mail-server'); // CHANGED from 'admin.settings'
     });
 
     Route::post('admin/mail-server/test', [AdminMailServerController::class, 'testSendEmail'])->name('admin.mail-server.test');
