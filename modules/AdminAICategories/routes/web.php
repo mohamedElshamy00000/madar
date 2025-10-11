b7613e4ea53670cf5584ad284dd81b5caf51c298
mohamed elshamy <64151860+mohamedElshamy00000@users.noreply.github.com>
2025-10-11 21:36:21 +0300

    Fix: Resolve duplicate route name for admin.ai.categories.update

--- modules/AdminAICategories/routes/web.php
+++ modules/AdminAICategories/routes/web.php
@@ -8,7 +8,8 @@ Route::middleware(['web', 'auth'])->group(function () {
     Route::group(["prefix" => "admin"], function () {
         Route::group(["prefix" => "ai/categories"], function () {
             Route::resource('/', AdminAICategoriesController::class)->names('admin.ai.categories');
-            Route::post('update', [AdminAICategoriesController::class, 'update'])->name('admin.ai.categories.update');
+            // Route::post('update', [AdminAICategoriesController::class, 'update'])->name('admin.ai.categories.update');
+            Route::post('update', [AdminAICategoriesController::class, 'update'])->name('admin.someothermodule.categories.update');
             Route::post('save', [AdminAICategoriesController::class, 'save'])->name('admin.ai.categories.save');
             Route::post('list', [AdminAICategoriesController::class, 'list'])->name('admin.ai.categories.list');
             Route::post('status/{any}', [AdminAICategoriesController::class, 'status'])->name('app.ai.categories.status');
