e1fdcefe26eff41f3a94e34a99a5ec4aaab8decb
mohamed elshamy <64151860+mohamedElshamy00000@users.noreply.github.com>
2025-10-11 21:47:30 +0300

    Fix: Remove duplicate template routes

--- modules/AdminAITemplates/routes/web.php
+++ modules/AdminAITemplates/routes/web.php
@@ -7,11 +7,9 @@ Route::middleware(['web', 'auth'])->group(function () {
     Route::group(["prefix" => "admin"], function () {
         Route::group(["prefix" => "ai/templates"], function () {
             Route::resource('', AdminAITemplatesController::class)->names('admin.ai.templates');
-            Route::post('update', [AdminAITemplatesController::class, 'update'])->name('admin.ai.templates.update');
             Route::post('list', [AdminAITemplatesController::class, 'list'])->name('admin.ai.templates.list');
             Route::post('save', [AdminAITemplatesController::class, 'save'])->name('admin.ai.templates.save');
             Route::post('status/{any}', [AdminAITemplatesController::class, 'status'])->name('admin.ai.templates.status');
-            Route::post('destroy', [AdminAITemplatesController::class, 'destroy'])->name('admin.ai.templates.destroy');
         });
 
     });
