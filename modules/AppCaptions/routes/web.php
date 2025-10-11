d0115896992cd6e7b258d3fd5ff4a27ec22a8de8
mohamed elshamy <64151860+mohamedElshamy00000@users.noreply.github.com>
2025-10-11 22:22:39 +0300

    Fix: Refactor and remove duplicate caption routes

--- modules/AppCaptions/routes/web.php
+++ modules/AppCaptions/routes/web.php
@@ -7,22 +7,22 @@ use Modules\AppCaptions\Http\Controllers\AppCaptionsController;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
-Route::group(["prefix" => "app"], function () {
-    Route::group(["prefix" => "captions"], function () {
-        Route::resource('/', AppCaptionsController::class)->names('app.captions');
-        Route::post('update', [AppCaptionsController::class, 'update'])->name('app.captions.update');
-        Route::post('save', [AppCaptionsController::class, 'save'])->name('app.captions.save');
-        Route::post('list', [AppCaptionsController::class, 'list'])->name('app.captions.list');
-        Route::post('list/popup', [AppCaptionsController::class, 'list'])->name('app.captions.popup_list');
-        Route::post('save_cation', [AppCaptionsController::class, 'saveCation'])->name('app.captions.save_cation');
-        Route::post('get_cation', [AppCaptionsController::class, 'getCation'])->name('app.captions.get_cation');
-        Route::post('destroy', [AppCaptionsController::class, 'destroy'])->name('app.captions.destroy');
-    });
+Route::group(["prefix" => "app/captions"], function () {
+    
+    // THIS IS THE CORRECTED CODE BLOCK
+    // هذا هو الكود الصحيح
+    
+    // This one line creates all standard routes: index, create, store, edit, update, destroy
+    // هذا السطر الواحد ينشئ كل الـ routes القياسية
+    Route::resource('/', AppCaptionsController::class)->names('app.captions');
+
+    // These are custom routes, so we keep them.
+    // هذه routes مخصصة، لذلك نحتفظ بها
+    Route::post('save', [AppCaptionsController::class, 'save'])->name('app.captions.save');
+    Route::post('list', [AppCaptionsController::class, 'list'])->name('app.captions.list');
+    Route::post('list/popup', [AppCaptionsController::class, 'list'])->name('app.captions.popup_list');
+    Route::post('save_cation', [AppCaptionsController::class, 'saveCation'])->name('app.captions.save_cation');
+    Route::post('get_cation', [AppCaptionsController::class, 'getCation'])->name('app.captions.get_cation');
 });
