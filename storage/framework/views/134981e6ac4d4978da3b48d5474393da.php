e79f49fc3b2d49fe3fd669c4e2a4faa268377158
mohamed elshamy <64151860+mohamedElshamy00000@users.noreply.github.com>
2025-10-11 23:27:38 +0300

    Fix: Correct theme folder structure for nova

--- storage/framework/views/134981e6ac4d4978da3b48d5474393da.php
+++
@@ -1,8 +0,0 @@
-<div class="d-flex">
-    <div class="form-control form-control-sm">
-        <button class="btn btn-icon">
-            <i class="fa-duotone fa-solid fa-magnifying-glass"></i>
-        </button>
-        <input name="<?php echo e($searchField); ?>" placeholder="<?php echo e($placeholder); ?>" type="text"/>
-    </div>
-</div><?php /**PATH /home/u774605506/domains/mdar.cc/public_html/resources/themes/app/pico/resources/views/components/datatable_search.blade.php ENDPATH**/ ?>
--- storage/framework/views/271b300b48dc9401804c309dfdca37cf.php
+++
@@ -1,9 +0,0 @@
-<?php if(!empty($options)): ?>
-<div class="d-flex">
-    <select class="form-select form-select-sm datatable_filter" name="<?php echo e($name); ?>">
-        <?php $__currentLoopData = $options; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $option): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-            <option value="<?php echo e($option['value']); ?>"><?php echo e($option['label']); ?></option>
-        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-    </select>
-</div>
-<?php endif; ?><?php /**PATH /home/u774605506/domains/mdar.cc/public_html/resources/themes/app/pico/resources/views/components/datatable_select.blade.php ENDPATH**/ ?>
