c09b14597c56703fd44fe3bfbf2965d23bf79606
mohamed elshamy <64151860+mohamedElshamy00000@users.noreply.github.com>
2025-10-11 21:55:15 +0300

    Fix: Remove duplicate blog routes

--- modules/AdminBlogs/routes/web.php
+++ modules/AdminBlogs/routes/web.php
@@ -5,12 +5,18 @@ use Modules\AdminBlogs\Http\Controllers\AdminBlogsController;
 
 Route::middleware(['web', 'auth'])->group(function () {
     Route::group(["prefix" => "admin/blogs"], function () {
+        
+        // THIS IS THE CORRECTED CODE BLOCK
+        // هذا هو الكود الصحيح
+        
+        // This one line creates all standard routes: index, create, store, edit, update, destroy
+        // هذا السطر الواحد ينشئ كل الـ routes القياسية
         Route::resource('', AdminBlogsController::class)->names('admin.blogs');
+
+        // These are custom routes, so we keep them.
+        // هذه routes مخصصة، لذلك نحتفظ بها
         Route::post('list', [AdminBlogsController::class, 'list'])->name('admin.blogs.list');
-        Route::get('update', [AdminBlogsController::class, 'update'])->name('admin.blogs.update');
-        Route::get('edit/{id}', [AdminBlogsController::class, 'update'])->name('admin.blogs.edit');
         Route::post('save', [AdminBlogsController::class, 'save'])->name('admin.blogs.save');
         Route::post('status/{any}', [AdminBlogsController::class, 'status'])->name('admin.blogs.status');
-        Route::post('destroy', [AdminBlogsController::class, 'destroy'])->name('admin.blogs.destroy');
     });
 });
